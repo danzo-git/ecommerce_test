985e3ba21864e4c5edddd75e703b84f5cf7cec23
danzo-git <kouamedenzel09@gmail.com>
2021-12-24 12:00:48 +0000

    shop page avec produits dynamiques

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -7,6 +7,7 @@
 			<meta name="viewport" content="width=device-width, initial-scale=1">
 			<title>Home</title>	
 			<link rel="shortcut icon" type="image/x-icon" href="{{asset('assets/images/favicon.ico')}}">
+			<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css">
 			<link href="{{asset('https://fonts.googleapis.com/css?family=Lato:300,400,400italic,700,700italic,900,900italic&amp;subset=latin,latin-ext')}}" rel="stylesheet">
 			<link href="{{asset('https://fonts.googleapis.com/css?family=Open%20Sans:300,400,400italic,600,600italic,700,700italic&amp;subset=latin,latin-ext')}}" rel="stylesheet">
 			<link rel="stylesheet" type="text/css" href="{{asset('assets/css/animate.css')}}">
